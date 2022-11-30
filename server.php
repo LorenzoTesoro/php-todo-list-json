3632c316592108e0d0853b695de2341ab506280f
LorenzoTesoro <lorenzotesoro2@gmail.com>
2022-11-30 17:02:51 +0100

    add writing json

--- server.php
+++ server.php
@@ -7,3 +7,10 @@ $tasks = json_decode($string, true);
 header('Content-Type: application/json');
 
 echo json_encode($tasks);
+
+if (isset($_POST)) {
+
+    $json_string = json_encode($_POST['new_task']);
+
+    file_put_contents('list.json', $json_string);
+}
